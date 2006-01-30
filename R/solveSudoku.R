225914d06a67138088076b2348e8c0b6ad76c7a8
David Brahm <brahm@alum.mit.edu>
2006-01-30 00:00:00 +0000

    version 2.0

--- R/solveSudoku.R
+++ R/solveSudoku.R
@@ -1,9 +1,6 @@
-sudoku <- function(z, verbose=FALSE, map=c(1:9,letters), level=0, dir=".") {
-  if (length(z)==1) {                    # if "z" is a filename, parse the file
-    z <- scan(file.path(dir, z), "", quiet=TRUE)
-    z <- do.call(rbind, strsplit(z, ""))
-    z <- matrix(match(z, map, 0), nrow(z))
-  }
+solveSudoku <- function(z, verbose=FALSE, map=c(1:9,letters), level=0,
+                        print.it=TRUE) {
+  if (length(z)==1) z <- readSudoku(z, map)
   N <- nrow(z);  Ns <- sqrt(N)                              # Traditionally N=9
   oldngot <- sum(z > 0)
   if (verbose) cat("Known:", oldngot, ", level:", level, "\n")
@@ -13,11 +10,13 @@ sudoku <- function(z, verbose=FALSE, map=c(1:9,letters), level=0, dir=".") {
     if (length(i)!=1 || length(j)!=1 || length(k)!=1) {isok<<-FALSE; return()}
     if (verbose && txt != "") cat(i, j, "=", k, txt, "\n")
     z[i,j] <<- k
+    ain <- a
     a[ i, j,  ] <<- seq(1:N)==k
     a[ i,-j, k] <<- FALSE                         # No other k's in this row
     a[-i, j, k] <<- FALSE                         # No other k's in this column
     for (ii in Ns*((i-1) %/% Ns) + 1:Ns) for (jj in Ns*((j-1) %/% Ns) + 1:Ns)
       if (!(ii==i && jj==j)) a[ii,jj, k] <- FALSE # No other k's in this box
+    if (any(a != ain, na.rm=TRUE)) isok <<- FALSE   # You turned a T into an F!
   }
   for (i in 1:N) for (j in 1:N) if (k <- z[i,j]) fill(i, j, k)
 
@@ -42,7 +41,10 @@ sudoku <- function(z, verbose=FALSE, map=c(1:9,letters), level=0, dir=".") {
     if (!isok) {if (verbose) cat("Inconsistent level", level, "\n"); return()}
     ngot <- sum(z > 0)
     if (verbose) cat("Known:", ngot, ", level:", level, "\n\n")
-    if (ngot==N^2) {print(matrix(map[z],N),,FALSE,,,TRUE);return(invisible(z))}
+    if (ngot==N^2) {
+      if (print.it) print(matrix(map[z],N), quote=FALSE, right=TRUE)
+      return(invisible(z))
+    }
     if (ngot==oldngot) {                               # Failed.  Take a guess!
       poss <- rowSums(is.na(a), ,2)                # Number of possible guesses
       if (!any(poss)) {if (verbose) cat("No possibilities left\n"); return()}
@@ -51,7 +53,7 @@ sudoku <- function(z, verbose=FALSE, map=c(1:9,letters), level=0, dir=".") {
       if (verbose) cat("Guessing:", ij[1], ij[2], "=", k, "\n")
       zg <- z
       zg[ij[1], ij[2]] <- k
-      res <- Recall(zg, verbose, map, level+1)
+      res <- Recall(zg, verbose, map, level+1, print.it)
       if (is.null(res)) a[ij[1], ij[2], k] <- FALSE else return(invisible(res))
     }
     oldngot <- ngot
